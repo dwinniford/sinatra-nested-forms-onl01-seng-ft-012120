8444480cf8975868a1afd7e818eb8f4e22b58e1f
Dave Winniford <dwinniford@gmail.com>
2020-02-19 20:12:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ module FormsLab
   
   post '/pirates' do 
     @pirate = Pirate.new(params[:pirate])
-    params[:ships].each do |ship|
+    params[:pirate][:ships].each do |ship|
       Ship.new(ship)
     end 
     @ships = Ship.all 
