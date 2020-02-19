3107baae73c1ddd2f1f9fe013377a14974e3db67
Dave Winniford <dwinniford@gmail.com>
2020-02-19 19:04:58 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,15 @@
 class Ship
   attr_accessor :name, :type, :booty
   @@all = []
+  
+  def initialize(hash)
+    @name = hash[:name]
+    @type = hash[:type]
+    @booty = hash[:booty]
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 end
